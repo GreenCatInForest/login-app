7f8bcbc3e01d59c75252daf74fab22e6c64c08cd
GreenCatInForest <event2016.2017@gmail.com>
2023-11-10 23:26:06 +0000

    add basic content for login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,29 @@
 export const Login = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div>
+      <form className="loginForm">
+        <label for="email" className="form-label">
+          E-mail
+        </label>
+        <input
+          name="email"
+          type="email"
+          id="email"
+          className="form-control"
+          placeholder="Enter your email address"
+        />
+        <label for="password" className="form-label">
+          Password
+        </label>
+        <input
+          name="password"
+          type="text"
+          id="password"
+          className="form-control"
+          placeholder="Enter your password"
+        />
+      </form>
+      <button type="submit">Login</button>
+    </div>
+  );
 };
