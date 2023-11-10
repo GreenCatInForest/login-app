e32022c869a11ef73aeb98282022611fc127af5b
GreenCatInForest <event2016.2017@gmail.com>
2023-11-10 23:17:13 +0000

    add basic content to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,11 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <h1>LF</h1>
+        <h2>Login Form</h2>
+      </div>
+      <h3>Type anything in the fields and see the result:</h3>
+    </div>
+  );
 };
