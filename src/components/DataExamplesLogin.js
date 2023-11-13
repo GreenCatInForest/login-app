3bf4cdfd136fbf36460381d4a9c4d6f281143946
GreenCatInForest <event2016.2017@gmail.com>
2023-11-13 22:30:05 +0000

    add responsive design

--- src/components/DataExamplesLogin.js
+++ src/components/DataExamplesLogin.js
@@ -1,16 +1,28 @@
+import emailIcon from "../assets/images/icons/icons8-email-50.png";
+import passwordIcon from "../assets/images/icons/icons8-password-50.png";
+
 export const DataExamplesLogin = () => {
   return (
     <div className="dataExamplesLogin">
       <h4 className="dataExamplesTitle">
         Try to login using examples, provided below:
       </h4>
+
       <div className="dataExamplesData">
-        <p>Email: JohnSnow@wall.co.north</p>
-        <p>Password: KnowNothingExceptTheNightKingisREAL1111</p>
-        <p>Email: Arya_no_one_stark@braavos.com</p>
-        <p>Password: StickThemWithThePoinEnd</p>
+        <img src={emailIcon} alt="" />
+        <p>JohnSnow@wall.co.north</p>
+        <img src={passwordIcon} alt="" />
+        <p>KnowNothingExceptTheNightKingisREAL1111</p>
+
+        <img src={emailIcon} alt="" />
+        <p>Arya_no_one_stark@braavos.com</p>
+        <img src={passwordIcon} alt="" />
+        <p>StickThemWithThePoinEnd</p>
+
+        <img src={emailIcon} alt="" />
         <p>Email: 1</p>
-        <p> Password:1</p>
+        <img src={passwordIcon} alt="" />
+        <p>Password: 1</p>
       </div>
     </div>
   );
